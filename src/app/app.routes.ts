4ae4fd5de19c0fdcf9dfe65080385fbd9976889b
SurajPawar15-smart <supersuraj143@gmail.com>
2025-11-06 08:13:40 +0530

    auth guard added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { Login } from '../pages/login/login';
 import { Layout } from '../pages/layout/layout';
 import { Master } from '../pages/master/master/master';
+import { authGuard } from '../core/guard/auth-guard';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: Login },
-  { path: '', component: Layout, children: [{ path: 'master', component: Master }] },
+  { path: '', component: Layout,//canActivate:[authGuard] ,
+    children: [{ path: 'master', component: Master }] },
 ];
